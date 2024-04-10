5484dd2327067d6ad9408ff2521028f801afce21
Junior331 <jarilson.rk@gmail.com>
2024-04-10 12:18:07 -0300

    Feat: implementing page SignUp

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,12 @@
 import { Navigate, Route, Routes } from 'react-router-dom';
-import { Home, SignIn } from '@/pages';
+import { Home, SignIn, SignUp } from '@/pages';
 
 export const AppRoutes = () => {
   return (
     <Routes>
       <Route index element={<SignIn />} />
       <Route path="/home" element={<Home />} />
+      <Route path="/signUp" element={<SignUp />} />
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
