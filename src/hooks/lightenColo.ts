e76673bdc8d58f668905ec965a03b9ae99bd85d1
Junior331 <jarilson.rk@gmail.com>
2024-04-18 19:24:48 -0300

    Fix: adjusting error build

--- src/hooks/lightenColo.ts
+++ src/hooks/lightenColo.ts
@@ -19,7 +19,7 @@ const useColorManipulation = ({
         const shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
         const fullHex = hex.replace(
           shorthandRegex,
-          (m, r, g, b) => r + r + g + g + b + b
+          (r, g, b) => r + r + g + g + b + b
         );
         const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(
           fullHex
