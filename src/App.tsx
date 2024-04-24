0832fd09b92e064fa72758305489280bde17f515
Junior331 <jarilson.rk@gmail.com>
2024-04-24 10:42:37 -0300

    Fix: removed line code

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import { AppRoutes } from './routes';
 import Provider from '@/state/provider';
 import Theme from '@/styles/Theme';
-import 'react-step-progress/dist/index.css';
 
 function App() {
   return (
